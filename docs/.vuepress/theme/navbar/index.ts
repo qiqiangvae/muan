74c5cd96e050b2270fd8c99b82a9a5d03a2ea94c
qiqiang <qiqiang>
2023-01-30 10:30:36 +0800

    fix

--- docs/.vuepress/theme/navbar/index.ts
+++ docs/.vuepress/theme/navbar/index.ts
@@ -3,7 +3,7 @@ import {navbar} from "vuepress-theme-hope";
 export const myNavbar = navbar([
     {text: "首页", link: "/", icon: "shouye"},
     {text: "开发技术", icon: "ruanjiankaifabao", link: "/dev/271d86"},
-    {text: "Java", icon: "java", link: "/java/", activeMatch: "/java"},
+    {text: "Java", icon: "java", link: "/java/toolUsage/79030a/", activeMatch: "/java"},
     {text: "云原生", icon: "kubernetes", link: "/cloudNative/", activeMatch: "/cloudNative"},
     {text: "Linux", icon: "linux", link: "/linux/db53d7"},
     {text: "中间件", icon: "zhongjianjian", link: "/middleware/", activeMatch: "/middleware"},
