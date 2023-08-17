a513eaeb6fb36a8422b7340541c145fa064770f2
Quince <qiqiangvae@foxmail.com>
2023-08-17 22:40:09 +0800

    fix

--- docs/.vuepress/theme/navbar/index.ts
+++ docs/.vuepress/theme/navbar/index.ts
@@ -2,10 +2,10 @@ import {navbar} from "vuepress-theme-hope";
 
 export const myNavbar = navbar([
     {text: "首页", link: "/", icon: "shouye"},
-    {text: "开发技术", icon: "ruanjiankaifabao", link: "/dev/271d86"},
-    {text: "Java", icon: "java", link: "/java/toolUsage/79030a/", activeMatch: "/java"},
-    {text: "云原生", icon: "kubernetes", link: "/cloudNative/", activeMatch: "/cloudNative"},
-    {text: "Linux", icon: "linux", link: "/linux/db53d7"},
-    {text: "中间件", icon: "zhongjianjian", link: "/middleware/", activeMatch: "/middleware"},
-    {text: "学习笔记", icon: "note", link: "/note/systemArchitecture/hardware/", activeMatch: "/note"},
+    {text: "开发技术", icon: "ruanjiankaifabao", link: "/dev/271d86", activeMatch: "^/dev"},
+    {text: "Java", icon: "java", link: "/java/toolUsage/79030a/", activeMatch: "^/java"},
+    {text: "云原生", icon: "kubernetes", link: "/cloudNative/", activeMatch: "^/cloudNative"},
+    {text: "Linux", icon: "linux", link: "/linux/db53d7", activeMatch: "^/linux"},
+    {text: "中间件", icon: "zhongjianjian", link: "/middleware/", activeMatch: "^/middleware"},
+    {text: "学习笔记", icon: "note", link: "/note/systemArchitecture/hardware/", activeMatch: "^/note"},
 ]);
